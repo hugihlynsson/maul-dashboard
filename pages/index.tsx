36c82dc04c504dc123c842b3842179167b9af8e2
Hugi Hlynsson <hugihlynsson@gmail.com>
2020-08-14 22:11:50 +0000

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>Maul Avo Reykjavík</title>
+      </Head>
+
+      <h1 className={styles.title}>Maul Avo Reykjavík</h1>
+    </main>
+  );
+}
